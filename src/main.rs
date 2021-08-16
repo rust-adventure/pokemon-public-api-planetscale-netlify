ff2212df3c278aae955fa111abd3ca24a3a4d67e
Christopher Biscardi <chris@christopherbiscardi.com>
2021-08-16 08:58:57 -0700

    query the db

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 extern crate diesel;
 
+use std::env;
+
+use diesel::prelude::*;
+
 mod models;
 mod schema;
 use models::*;
@@ -10,6 +14,7 @@ use lamedh_http::{
     lambda::{lambda, Context, Error},
     IntoResponse, Request,
 };
+use schema::pokemon_table::dsl::*;
 
 #[lambda(http)]
 #[tokio::main]
@@ -18,5 +23,21 @@ async fn main(
     _: Context,
 ) -> Result<impl IntoResponse, Error> {
     dbg!("in main");
+    let database_url =
+        env::var("DATABASE_URL").expect("a db url");
+    let connection = MysqlConnection::establish(
+        &database_url,
+    )
+    .expect(&format!(
+        "Error connecting to {}",
+        database_url
+    ));
+
+    let results = pokemon_table
+        .limit(5)
+        .load::<PokemonDB>(&connection)
+        .expect("Error loading pokemon");
+
+    dbg!(results);
     Ok("boop")
 }
